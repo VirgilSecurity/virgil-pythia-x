b3add965783682e066120d3b6e07c27ac5ab754d
Oleksandr Deundiak <Deundiak@gmail.com>
2018-04-04 17:55:37 +0300

    Added empty framework

---
+++ Source/VirgilSDKPythia.h
@@ -0,0 +1,19 @@
+//
+//  VirgilSDKPythia.h
+//  VirgilSDKPythia
+//
+//  Created by Oleksandr Deundiak on 4/4/18.
+//  Copyright © 2018 Virgil Security. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirgilSDKPythia.
+FOUNDATION_EXPORT double VirgilSDKPythiaVersionNumber;
+
+//! Project version string for VirgilSDKPythia.
+FOUNDATION_EXPORT const unsigned char VirgilSDKPythiaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirgilSDKPythia/PublicHeader.h>
+
+

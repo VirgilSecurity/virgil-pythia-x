ec8a93296e2813569274072343bd9e507e5f8bfc
Oleksandr Deundiak <Deundiak@gmail.com>
2018-05-21 13:28:46 +0300

    Added sleep to tests to avoid throttling

--- Tests/VSY001_BrainKeyTests.swift
+++ Tests/VSY001_BrainKeyTests.swift
@@ -97,8 +97,11 @@ class VSY001_BrainKeyTests: XCTestCase {
         let brainKey = BrainKey(context: brainKeyContext)
         
         let keyPair1 = try! brainKey.generateKeyPair(password: "some password").startSync().getResult()
+        sleep(3)
         let keyPair2 = try! brainKey.generateKeyPair(password: "some password").startSync().getResult()
+        sleep(3)
         let keyPair3 = try! brainKey.generateKeyPair(password: "another password").startSync().getResult()
+        sleep(3)
         let keyPair4 = try! brainKey.generateKeyPair(password: "some password", brainKeyId: "my password 1").startSync().getResult()
         
         XCTAssert(keyPair1.publicKey.identifier == keyPair2.publicKey.identifier)

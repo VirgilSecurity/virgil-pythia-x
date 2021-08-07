618cd2d17620e5179139fd6c8da97b3cb34c2076
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-07 16:25:23 +0300

    Fixed publish carthage ci step

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -4,5 +4,5 @@ carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
 FRAMEWORKS_PATH=Carthage/Build
-find ${FRAMEWORKS_PATH} ! -name 'VirgilSDKPythia.xcframework' -delete
+find ${FRAMEWORKS_PATH} -mindepth 1 -maxdepth 1 ! -name 'VirgilSDKPythia.xcframework' -exec rm -rvf {} \;
 zip -r VirgilSDKPythia.xcframework.zip ${FRAMEWORKS_PATH}

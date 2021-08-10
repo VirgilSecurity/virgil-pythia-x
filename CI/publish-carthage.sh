faf4605e00b8beb0f8c8ce99f4a09c13ec7d5f25
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-10 22:46:13 +0300

    Simplify & fix publish-carthage script

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -3,6 +3,4 @@ brew outdated carthage || brew upgrade carthage;
 carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
-FRAMEWORKS_PATH=Carthage/Build
-find ${FRAMEWORKS_PATH} -mindepth 1 -maxdepth 1 ! -name 'VirgilSDKPythia.xcframework' -exec rm -rvf {} \;
-zip -r VirgilSDKPythia.xcframework.zip ${FRAMEWORKS_PATH}
+zip -r VirgilSDKPythia.xcframework.zip Carthage/Build/VirgilSDKPythia.xcframework

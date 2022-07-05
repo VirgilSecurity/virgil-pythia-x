d66863e7775a7a7c38333f1c2d8809cbadcdf4e0
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2022-07-05 22:43:06 +0300

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VirgilSDKPythia",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "VirgilSDKPythia",
+            targets: ["VirgilSDKPythia"]),
+    ],
+
+    dependencies: [
+        .package(url: "https://github.com/VirgilSecurity/virgil-sdk-x.git", branch: "develop")
+    ],
+
+    targets: [
+        .target(
+            name: "VirgilSDKPythia",
+            dependencies: [
+                .product(name: "VirgilSDK", package: "virgil-sdk-x"),
+            ],
+            path: "Source"
+        )
+    ]
+)
